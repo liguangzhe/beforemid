4dff274a65d2a2bd30fafaabef699ff1142d1194
lgz0012 <gl279@njit.edu>
2018-10-23 22:44:19 -0400

    homepage,aboutpage,contactpage were been setup correctly

---
+++ resources/views/includes/mainNav.blade.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class navbar-header>
+            <a class="navbar-brand" href="#">WebSiteName</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="{{route('home')}}">Home</a></li>
+            <li><a href="/about"></a>About</li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+    </div>
+</nav>
---
+++ resources/views/pages/thankyou.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>thank you:{{$name}}</h1>
+@endsection
